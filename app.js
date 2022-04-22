2ba5b27219bf32ccc32ef5b8cd079fa0f8a79db2
Developer-Barsha <lamiatabassumbarsha@gmail.com>
2022-04-22 12:59:57 +0600

    problem solved

--- app.js
+++ app.js
@@ -14,18 +14,22 @@ setInterval(()=>{
 })
 
 // digital clock
-let hours = document.getElementById('hours')
-let minutes = document.getElementById('minutes')
-let seconds = document.getElementById('seconds')
-let ampm = document.getElementById('ampm')
+let hours = document.getElementById('hours');
+let minutes = document.getElementById('minutes');
+let seconds = document.getElementById('seconds');
+let ampm = document.getElementById('ampm');
 
-let h = new Date().getHours();
-let m = new Date().getMinutes();
-let s = new Date().getSeconds();
-
-// add 0 before 1 digit number
-
-
-hours.innerHTML = h;
-minutes.innerHTML = m;
-seconds.innerHTML = s;
+setInterval(()=>{
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    
+    // add 0 before 1 digit number
+    h = (h < 10) ? '0' + h : h;
+    m = (m < 10) ? '0' + m : m;
+    s = (s < 10) ? '0' + s : s;
+    
+    hours.innerHTML = h;
+    minutes.innerHTML = m;
+    seconds.innerHTML = s;
+})
